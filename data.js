ce95ba882b1254a4288fda27f457f2706ae66323
islomkarimovgood-create <islomkarimovgood@gmail.com>
2026-02-01 10:31:42 +0500

    Create GAMES array with game details
    Added a list of game objects with slugs and icons.

---
+++ data.js
@@ -0,0 +1,16 @@
+const GAMES = [
+  {
+    slug: "moto-racing",
+    icon: "images/moto.png"
+  },
+  {
+    slug: "zombie-shooter",
+    icon: "images/zombie.png"
+  },
+  {
+    slug: "car-drift",
+    icon: "images/car.png"
+  }
+];
+
+// 👆 добавляешь новые игры только сюда
