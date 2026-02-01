3f6ecbced89717e9dc7ee1c09fa28b5a00c4937a
islomkarimovgood-create <islomkarimovgood@gmail.com>
2026-02-01 10:32:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-const games = [
-  { name: "Moto Racing", url: "#" },
-  { name: "Zombie Shooter", url: "#" },
-  { name: "Stickman Fight", url: "#" },
-  { name: "Car Drift", url: "#" },
-  { name: "Tower Defense", url: "#" },
-  { name: "Puzzle Blocks", url: "#" },
-  { name: "Sniper Elite", url: "#" },
-  { name: "Basketball Pro", url: "#" }
-];
-
-// потом можно добавить до 100+
-
 const container = document.getElementById("games");
-const search = document.getElementById("search");
 
-function render(list) {
+function render() {
   container.innerHTML = "";
 
-  list.forEach(game => {
+  GAMES.forEach(game => {
     const div = document.createElement("div");
     div.className = "game";
-    div.innerHTML = `<h3>${game.name}</h3>`;
-    div.onclick = () => window.open(game.url, "_blank");
+
+    div.innerHTML = `
+      <img src="${game.icon}" loading="lazy">
+    `;
+
+    div.onclick = () => {
+      window.location.href = `games/${game.slug}.html`;
+    };
+
     container.appendChild(div);
   });
 }
 
-render(games);
-
-search.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase();
-  render(games.filter(g => g.name.toLowerCase().includes(value)));
-});
+render();
